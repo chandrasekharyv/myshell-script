bb384a7f6ea6383d068fa96ce5ce7a916f022927
chandrasekharyv <yvchandrasekhar418@gmail.com>
2023-08-16 21:12:05 +0530

    arrays

--- array.sh
+++ array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 array=("raj" "ram" "rohan")
-echo "1st person: ${array[0]})"
-echo "2nd person: ${array[1]})"
-echo "last person: ${array[2]})"
-echo "all persons: ${array[@]})"
+echo "1st person: ${array[0]}"
+echo "2nd person: ${array[1]}"
+echo "last person: ${array[2]}"
+echo "all persons: ${array[@]}"
