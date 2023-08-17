70532fd4a4ab97cf1f705fd48fbacacd2e89a0ed
chandrasekharyv <yvchandrasekhar418@gmail.com>
2023-08-17 18:47:13 +0530

    if condition checking root user permissions

--- 07-condition.sh
+++ 07-condition.sh
@@ -6,8 +6,8 @@ USERID=$(id -u)
 if ($USERID -ne 0)
 then
     echo "ERROR: Please give the root user permission"
-else
-    echo "INFO: you can install sofware"
+# else
+#     echo "INFO: you can install sofware"
 fi
 
 yum install mysql -y
