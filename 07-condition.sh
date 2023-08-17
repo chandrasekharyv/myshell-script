90e74307e189e79fa3c07b282aa991e15d25df7d
chandrasekharyv <yvchandrasekhar418@gmail.com>
2023-08-17 18:50:16 +0530

    if condition checking root user permissions

--- 07-condition.sh
+++ 07-condition.sh
@@ -3,7 +3,7 @@
 #if not a root user , eit the program and inform user to run with root user
 #if root user install  mysql 
 USERID=$(id -u)
-if ($USERID -ne 0)
+if [ $USERID -ne 0 ]
 then
     echo "ERROR: Please give the root user permission"
 # else
